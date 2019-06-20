9d68e4f3e4e86dc7df56d72722c841eb76957d47
shannonbee <shannonbenyamin@gmail.com>
2019-06-20 16:23:19 -0400

    Iteration One

--- shadyShannon/ViewController.swift
+++ shadyShannon/ViewController.swift
@@ -7,14 +7,16 @@
 //
 
 import UIKit
+        class ViewController: UIViewController {
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+@IBAction func showMessage(sender: UIButton) {
+    
+    // in as much detail as possible
+    // pseudo-code the things that need to happen
+    // when a user clicks an emoji button
+    
+}
 
 }
 
