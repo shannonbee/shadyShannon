184d00edf0ca33c4e46d88d67d8af68958283228
shannonbee <shannonbenyamin@gmail.com>
2019-06-21 16:34:03 -0400

    Iteration 2

--- shadyShannon/ViewController.swift
+++ shadyShannon/ViewController.swift
@@ -7,16 +7,40 @@
 //
 
 import UIKit
-        class ViewController: UIViewController {
-
-    }
+    class ViewController: UIViewController {
+    let emojis = ["🙄" : " rolling eyes ","😎" : "sunglasses", "🌴"  : "palm tree", "🌳" : "Big Tree" ]
+        let customMessages = [
+            "rolling eyes": ["I am busy right now , can I ignore you some other time?", "Your name isn't Google so stop acting like you know everything"],
+            "sunglasses": [ "Sure, I'd love to help you out... now, which way did you come in?" , "If I'm driving you crazy just remember to put on your seatbelt "],
+            "palm tree":[ "The shade from this tree is brighter than your future" , ""
+            ]
+                            []
+        
+        
+        ]
+// ["🙄" : " I am busy right now , can I ignore you some other time?","😎" : "Sure, I'd love to help you out... now, which way did you come in?","🌴" : "The shade from this tree is brighter than your future", "🌳" : "I would say I outdid myself, but I'm always this good. So I simply did myself" ]
 @IBAction func showMessage(sender: UIButton) {
-    
-    // in as much detail as possible
-    // pseudo-code the things that need to happen
-    // when a user clicks an emoji button
-    
-}
+    let selectedEmotion = sender.titleLabel?.text
+        //let messageOptions = [emojis[selectedEmotion!]!].count
 
+    let alertController = UIAlertController(title: "Hola", message: emojis[selectedEmotion!], preferredStyle: UIAlertController.Style.alert)
+     alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+    
+    present(alertController, animated: true, completion: nil)
+    
 }
+}
+    
+    
+
+    
+    // the rest of your code from Iteration 1
+    
+
+
+
+
+    
+    // when user clicks on emoji a message should pop up
+    // connect all emojis to the // when a user clicks an emoji butyton
 
